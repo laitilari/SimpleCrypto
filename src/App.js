3e22646beef3f492aaaa2530e617d120ecc887f9
Ilari Laitinen <iim.laitinen@gmail.com>
2019-10-07 20:52:41 +0300

    fetch fsn price and round it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
 import './App.css';
+import * as Round from '../node_modules/round-to'
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { btcEur: [] };
+    this.state = { btcEur: '', fsnEur: '' };
   }
 
   async componentDidMount() {
     try {
       const response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/EUR`);
       const json = await response.json();
-      const btcEur = json.bpi.EUR.rate.slice(0, -5) + ' €'
+      const btcEur = json.bpi.EUR.rate + ' €';
       this.setState({ btcEur: btcEur });
+    } catch (error) {
+      alert(error)
+    }
+
+    try {
+      const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=fsn&vs_currencies=eur`);
+      // console.log(await response.text()) 
+      const text = await response.text();
+      const json = JSON.parse(text);
+      const fsnEur = Round(json.fsn.eur, 2) + ' €'
+      this.setState({ fsnEur: fsnEur });
     } catch (error) {
       console.log(error)
     }
@@ -21,7 +33,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        BTC = {this.state.btcEur}
+        BTC = {this.state.btcEur} <br></br>
+        FSN = {this.state.fsnEur}
       </div>
     );
   }
