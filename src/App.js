ce2bda0bc1dc267646bc8e849d323e785149b831
Ilari Laitinen <iim.laitinen@gmail.com>
2019-11-05 22:45:17 +0200

    get currencies prices and show on ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import './App.css';
+import './App.scss';
 import * as Round from '../node_modules/round-to'
+// import bitcoin from '../public/bitcoin'
 
 class App extends Component {
   constructor() {
@@ -12,15 +13,14 @@ class App extends Component {
     try {
       const response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/EUR`);
       const json = await response.json();
-      const btcEur = json.bpi.EUR.rate + ' €';
+      const btcEur = Round(Number(json.bpi.EUR.rate.split(',').join('')), 2) + ' €';
       this.setState({ btcEur: btcEur });
     } catch (error) {
-      alert(error)
+      console.log(error)
     }
 
     try {
       const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=fsn&vs_currencies=eur`);
-      // console.log(await response.text()) 
       const text = await response.text();
       const json = JSON.parse(text);
       const fsnEur = Round(json.fsn.eur, 2) + ' €'
@@ -28,16 +28,62 @@ class App extends Component {
     } catch (error) {
       console.log(error)
     }
+
+    try {
+      const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=hedera-hashgraph&vs_currencies=eur`);
+      const text = await response.text();
+      const json = JSON.parse(text);
+      const hbarEur = Round(json["hedera-hashgraph"].eur, 4) + ' €'
+      this.setState({ hbarEur: hbarEur });
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async getGoinsList() {
+    const response = await fetch(`https://api.coingecko.com/api/v3/coins/list`);
+    const text = await response.text();
+    const json = JSON.parse(text);
+    console.log(json)
+    return json
   }
 
   render() {
     return (
       <div className="app">
-        BTC = {this.state.btcEur} <br></br>
-        FSN = {this.state.fsnEur}
+        <div className="container">
+          <div>
+            <img className="btc-logo"
+              src={require("./images/btc.svg")}
+              alt="bitcoin logo"
+            />
+            <CryptoPrice crypto={this.state.btcEur} />
+          </div>
+          <div>
+            <img className="fsn-logo"
+              src={require("./images/fsn.png")}
+              alt="fsn logo"
+            />
+            <CryptoPrice crypto={this.state.fsnEur} />
+          </div>
+          <div>
+            <img className="hbar-logo"
+              src={require("./images/hbar.jpg")}
+              alt="hbar logo"
+            />
+            <CryptoPrice crypto={this.state.hbarEur} />
+          </div>
+        </div>
       </div>
     );
   }
+}
+
+const CryptoPrice = (props) => {
+  const { crypto } = props;
+  return (
+    <div>{crypto}</div>
+  )
 }
 
 export default App;
