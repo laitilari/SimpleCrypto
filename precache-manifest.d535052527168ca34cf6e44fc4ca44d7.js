b45e4e4ed84dc3afc121ec4a71fc461380b198a5
Ilari Laitinen <iim.laitinen@gmail.com>
2019-11-06 17:00:42 +0200

    Updates

---
+++ precache-manifest.d535052527168ca34cf6e44fc4ca44d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d3a0136f48a344cf6e742b8bdf1b07",
+    "url": "/simplecrypto/index.html"
+  },
+  {
+    "revision": "92605b1bbeadf8241050",
+    "url": "/simplecrypto/static/css/main.ef963a8f.chunk.css"
+  },
+  {
+    "revision": "76abdd2f53d1948c8b47",
+    "url": "/simplecrypto/static/js/2.9f4d3b99.chunk.js"
+  },
+  {
+    "revision": "92605b1bbeadf8241050",
+    "url": "/simplecrypto/static/js/main.86dd48a8.chunk.js"
+  },
+  {
+    "revision": "7f2684a80dbb566fdca7",
+    "url": "/simplecrypto/static/js/runtime-main.8139cde3.js"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/simplecrypto/static/media/btc.d7876d48.svg"
+  },
+  {
+    "revision": "1e38795cdaeebc30115025c69c0cbf95",
+    "url": "/simplecrypto/static/media/fsn.1e38795c.png"
+  }
+]);
